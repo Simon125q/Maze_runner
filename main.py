cf1af7e3aed770b11c71c54d4f08414894ef0686
Simon125q <sz.omiecinski@gmail.com>
2023-08-02 13:34:46 +0200

    add movement

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import pygame
 import sys
 from settings import *
 from map import Map
+from player import Player
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
     
     def upadate(self):
+        self.player.update()
         pygame.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pygame.display.set_caption("Maze runner")
         
     def draw(self):
         self.screen.fill('black')
         self.map.draw()
+        self.player.draw()
         
     def check_events(self):
         for event in pygame.event.get():
--- player.py
+++ player.py
@@ -9,7 +9,49 @@ class Player:
         self.angle = PLAYER_ANGLE
         
     def movement(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pygame.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pygame.K_a]:
+            dx += speed_sin
+            dy += speed_cos
+        if keys[pygame.K_d]:
+            dx += -speed_sin
+            dy += -speed_cos
+        
+        self.check_wall_collision(dx, dy)
+        
+        if keys[pygame.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pygame.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angle %= math.tau
+        
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+    
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
+        
+    def draw(self):
+        pygame.draw.line(self.game.screen, LIGHT_COLOR, (self.x * TILE, self.y * TILE),
+                         (self.x * TILE + WIDTH * math.cos(self.angle),
+                          self.y * TILE + WIDTH * math.sin(self.angle)), 2)
+        pygame.draw.circle(self.game.screen, PLAYER_COLOR, (self.x * TILE, self.y * TILE), 15)
     
     def update(self):
         self.movement()
